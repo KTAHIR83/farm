a74da53549ccc81f48384b55b164a2c74eb04282
tsibissolution <71134156+tsibissolution@users.noreply.github.com>
2025-11-29 22:23:36 +0530

    Update farm.js

--- farm.js
+++ farm.js
@@ -18,8 +18,8 @@ const api = axios.create({
 
 const plantSeed = [
   {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "690b47dd284c0906f5ae679d","seedIDs": "673e0c942c7bfd708b35247d","growthTimeMin": 2,"growthTime": 120000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "690733c4b2acdd963c39c167","seedIDs": "6900ddfce2a48c502f309993","growthTimeMin": 1440,"growthTime": 80100000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68f3a2583d1b80e2a90e2c15","seedIDs": "67dc227a59b878f195998dca","growthTimeMin": 15,"growthTime": 900000},
+  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "690733c4b2acdd963c39c167","seedIDs": "68824771915623f3dcc1fb09","growthTimeMin": 1440,"growthTime": 21600000},
+  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68f3a2583d1b80e2a90e2c15","seedIDs": "67dc227a59b878f195998dd0","growthTimeMin": 15,"growthTime": 900000},
   {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68c7b8915c457c186fd40041","seedIDs": "665f2698534176fcd32f9a7d","growthTimeMin": 30,"growthTime": 1800000},
   {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68dd8aa03bdf0c6e7893f8ed","seedIDs": "665f2698534176fcd32f9a7d","growthTimeMin": 30,"growthTime": 1800000},
   {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68e0ba678e6f03b23d39fc6a","seedIDs": "673e0c942c7bfd708b352405","growthTimeMin": 6, "growthTime": 360000},
@@ -164,6 +164,7 @@ async function startFarm() {
 }
 
 startFarm().catch(err => console.error("💥 Fatal error:", err.message));
+
 
 
 
