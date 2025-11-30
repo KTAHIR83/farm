439f2e68494a722b4728e321bcf8682cc6adada1
tsibissolution <71134156+tsibissolution@users.noreply.github.com>
2025-11-30 20:37:15 +0530

    Refactor plantSeed array and update bed cycle logic

--- farm.js
+++ farm.js
@@ -17,20 +17,18 @@ const api = axios.create({
 
 
 const plantSeed = [
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "690b47dd284c0906f5ae679d","seedIDs": "673e0c942c7bfd708b35247d","growthTimeMin": 2,"growthTime": 120000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "690733c4b2acdd963c39c167","seedIDs": "68824771915623f3dcc1fb09","growthTimeMin": 1440,"growthTime": 21600000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68f3a2583d1b80e2a90e2c15","seedIDs": "67dc227a59b878f195998dd0","growthTimeMin": 15,"growthTime": 900000},
+  
   {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68c7b8915c457c186fd40041","seedIDs": "665f2698534176fcd32f9a7d","growthTimeMin": 30,"growthTime": 1800000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68dd8aa03bdf0c6e7893f8ed","seedIDs": "665f2698534176fcd32f9a7d","growthTimeMin": 30,"growthTime": 1800000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68e0ba678e6f03b23d39fc6a","seedIDs": "673e0c942c7bfd708b352405","growthTimeMin": 6, "growthTime": 360000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68df7531bd36593b8b5ec737","seedIDs": "673e0c942c7bfd708b352465","growthTimeMin": 4,"growthTime": 240000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68dd8aa03bdf0c6e7893f901","seedIDs": "67dc227a59b878f195998db2","growthTimeMin": 32,"growthTime": 1920000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68dd8aa03bdf0c6e7893f8fd","seedIDs": "69285140f803f06b82ab22e6","growthTimeMin": 2,"growthTime": 14400000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68dd8aa03bdf0c6e7893f8f9","seedIDs": "673e0c942c7bfd708b35245f","growthTimeMin": 4,"growthTime": 240000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68dd8aa03bdf0c6e7893f8f5","seedIDs": "673e0c942c7bfd708b35245f","growthTimeMin": 4,"growthTime": 240000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68dd8aa03bdf0c6e7893f8f1","seedIDs": "67dc227a59b878f195998e7e","growthTimeMin": 8,"growthTime": 480000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68c2ed1e5c457c186fbcf096","seedIDs": "67dc227a59b878f195998d94","growthTimeMin": 13,"growthTime": 780000},
-  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "6922bd1d63bf2b69e4b7db7d","seedIDs": "673e0c942c7bfd708b35244d","growthTimeMin": 2,"growthTime": 120000}
+  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68dd8aa03bdf0c6e7893f8ed","seedIDs": "665f2698534176fcd32f9a7d","growthTimeMin": 30,"growthTime": 1800000},  
+  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68dd8aa03bdf0c6e7893f901","seedIDs": "673e0c942c7bfd708b352405","growthTimeMin": 32,"growthTime": 360000},
+  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68dd8aa03bdf0c6e7893f8fd","seedIDs": "673e0c942c7bfd708b35245f","growthTimeMin": 32,"growthTime": 240000},
+  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68e0ba678e6f03b23d39fc6a","seedIDs": "673e0c942c7bfd708b352465","growthTimeMin": 32,"growthTime": 240000},
+  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "690b47dd284c0906f5ae679d","seedIDs": "673e0c942c7bfd708b3524f3","growthTimeMin": 32,"growthTime": 120000},
+  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "692c56ded5279610094c3213","seedIDs": "68824771915623f3dcc1fb09","growthTimeMin": 32,"growthTime": 21600000},
+  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "692c56f964e8f74db4acd14f","seedIDs": "67dc227a59b878f195998db2","growthTimeMin": 32,"growthTime": 1920000},
+  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "692c571fd5279610094c548f","seedIDs": "673e0c942c7bfd708b352423","growthTimeMin": 32,"growthTime": 1020000},
+  {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "692c575064e8f74db4ad0094","seedIDs": "673e0c942c7bfd708b352447","growthTimeMin": 32,"growthTime": 120000},
+  
 ]
 
 
@@ -164,6 +162,7 @@ async function startFarm() {
 }
 
 startFarm().catch(err => console.error("💥 Fatal error:", err.message));
+
 
 
 
